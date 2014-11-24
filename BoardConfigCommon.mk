9a881a42476105c96a8e93f49c31dbd11922797c
martincz <martincz@mokeedev.com>
2014-11-25 03:04:24 +0800

    mokee bringup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 The MoKee OpenSource Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -76,7 +76,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 # CM Hardware
-BOARD_HARDWARE_CLASS := device/sony/rhine-common/cmhw
+BOARD_HARDWARE_CLASS := device/sony/rhine-common/mkhw
 
 # Dumpstate
 BOARD_LIB_DUMPSTATE := libdumpstate.sony
